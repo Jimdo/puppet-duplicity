cdd761e72d7239f8075aae64b563434be8c4f9c2
Ilya Margolin <ilya@jimdo.com>
2014-10-20 17:13:32 +0200

    Install packages via stdlib's ensure_packages
    so packages do not cause a conflict between modules

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -1,6 +1,6 @@
 class duplicity::packages {
   # Install the packages
-  package {
-    ['duplicity', 'python-boto', 'gnupg']: ensure => present
-  }
+  ensure_packages(
+    ['duplicity', 'python-boto', 'gnupg']
+  )
 }
